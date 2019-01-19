82d7769ebbbf40a39243f7bed542f64a9eb6bac1
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2019-01-19 11:26:55 +0100

    sql

--- zajecia1.sql
+++ zajecia1.sql
@@ -4,6 +4,8 @@ select distinct productScale from products order by productScale desc;
 
 select COUNT(distinct productScale) from products;
 
+select COUNT(distinct productVendor) from products;
+
 select COUNT(distinct employeeNumber) from employees;
 
 
