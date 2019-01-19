84f27a2841e44fc67128ee83971528db32f9395c
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2019-01-19 11:24:10 +0100

    zadania 1

---
+++ zajecia1.sql
@@ -0,0 +1,9 @@
+select distinct productName, productVendor,MSRP from products;
+
+select distinct productScale from products order by productScale desc;
+
+select COUNT(distinct productScale) from products;
+
+select COUNT(distinct employeeNumber) from employees;
+
+
