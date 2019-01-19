e97cdd5b20fc54f36bf2d1ba57f61824887bd390
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2019-01-19 12:01:13 +0100

    Filtering data

--- zajecia1.sql
+++ zajecia1.sql
@@ -1,3 +1,5 @@
+/*standard querries*/
+
 select distinct productName, productVendor,MSRP from products;
 
 select distinct productScale from products order by productScale desc;
@@ -7,5 +9,21 @@ select COUNT(distinct productScale) from products;
 select COUNT(distinct productVendor) from products;
 
 select COUNT(distinct employeeNumber) from employees;
+
+/*Filtering data
+SELECT list FROM table WHERE conditions*/
+
+SELECT customerName,creditLimit FROM customers WHERE country='Poland' OR country='Germany' AND creditLimit > 10000;
+
+SELECT productName,productScale,MSRP FROM products WHERE productVendor like '%66%';
+
+SELECT * FROM products WHERE productScale='1:10' OR productScale='1:700';
+
+SELECT * FROM products WHERE quantityInStock < 1000 AND buyPrice > 50;
+
+
+
+
+
 
 
