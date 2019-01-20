be42fe3f12fe6e56ebf113b419be815e51bd3c50
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2019-01-20 13:37:35 +0100

    filling data

---
+++ fillDataMoviesRental.sql
@@ -0,0 +1,81 @@
+/*Cwiczenie 15
+
+1. Zmodyfikuj tabelę rents tak, aby domyślną wartością dla pola status było ‘In progress’.
+2. Uzupełnij bazę danych przykładowymi danymi, dodaj: 3 ulubione filmy, kilka ich fizycznych kopii,
+przykładowego klienta, 2 wypożyczenia. Dla spójności, wprowadzaj dane po angielsku. Ułatw sobie życie,
+skorzystaj z bazy IMDB: https://www.imdb.com/title/tt5491994/ Sprawdź, czy komendy poprawnie się
+wykonują i podziel się z nimi z grupą na Slacku. UWAGA: jeśli skorzystaliście z mojego skryptu tworzącego
+tabele, będziecie mieć 2 błędy logiczne – zidentyfikujcie je I poprawcie poprzez ALTER TABLE.
+
+Cw 15 - ALTER TABLE:
+1) W tabeli moviesCopies pole rentedTo musi byc zdefiniowane jako klucz obcy do tabeli customers, pole customerId
+2) W tabeli rents - zgodnie z poleceniem pp 1
+3) W tabeli rents - zmienic price na (4,2)
+ad 1 -> ALTER TABLE Orders
+ADD FOREIGN KEY (PersonID) REFERENCES Persons(PersonID);
+*/
+
+ALTER TABLE moviesCopies
+ADD FOREIGN KEY (rentedTo) REFERENCES customers(customerId);
+
+ALTER TABLE rents ALTER status SET DEFAULT 'In rent';
+
+ALTER TABLE rents
+MODIFY rentPricePerDay DECIMAL(4,2) NOT NULL;rentPricePerDay
+
+ALTER TABLE moviesCopies MODIFY rentedTo INT(7) NULL;
+
+INSERT INTO moviesInfo(title,genre,releaseDate,description)
+VALUES
+('Mad Max: Fury Road',
+'Sci-Fi',
+CAST('2015-01-01' as DATE),
+'In a post-apocalyptic wasteland, a woman rebels against a tyrannical ruler in search for her homeland with the aid of a group of female prisoners, a psychotic worshiper, and a drifter named Max.'
+);
+
+INSERT INTO moviesInfo(title,genre,releaseDate,description)
+VALUES
+('Alien: Covenant',
+'Sci-Fi',
+CAST('2017-05-19' as DATE),
+'The crew of a colony ship, bound for a remote planet, discover an uncharted paradise with a threat beyond their imagination, and must attempt a harrowing escape.'
+)
+('Mad Max: Fury Road',
+'Sci-Fi',
+CAST('2015-05-15' as DATE),
+'In a post-apocalyptic wasteland, a woman rebels against a tyrannical ruler in search for her homeland with the aid of a group of female prisoners, a psychotic worshiper, and a drifter named Max.'
+)
+;
+
+#insert copies
+INSERT INTO moviescopies(movieInfoId)
+VALUES
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(2),
+(2),
+(2)
+;
+
+INSERT INTO customers(fullName,phone,email,address)
+VALUES
+('Grzegorz Chrzaszczyk','+48123123123','email@email.com','Warsaw Bielany'),
+('Adam Malysz','+48321321321','adam@malysz.pl','Wisla Malinka')
+;
+
+
+INSERT INTO rents(rentedMovieId,customer,status,rentPricePerDay,rentedDate,returnedDate)
+VALUES
+(1,1,'In rent','1.80',20190119,NULL),
+(1,1,'In rent','2.20',curdate(),NULL),
+(2,2,'In rent','2.99',curdate(),NULL)
+;
+
+
+
+
+
--- moviesRentalDefault.sql
+++ moviesRentalDefault.sql
@@ -3,25 +3,6 @@ CREATE DATABASE IF NOT EXISTS moviesRental; #operujemy na nowo utworzonej bazie 
 USE moviesRental;
 
 #wazna kolejnosc tabel ze wzgledu na referowanie sie do kluczy obcych
-CREATE TABLE IF NOT EXISTS moviesInfo (
-movieInfoId INT(7) NOT NULL AUTO_INCREMENT, #PK dla tej tabeli
-title VARCHAR(511) NOT NULL,
-genre VARCHAR(127) NOT NULL,
-releaseDate DATE NOT NULL,
-description TEXT, #uznalem ze moze byc nullem
-PRIMARY KEY (movieInfoId) #PK dla tej kolumny
-);
-
-CREATE TABLE IF NOT EXISTS moviesCopies (
-copyId INT(7) NOT NULL AUTO_INCREMENT,
-movieInfoId INT(7) NOT NULL,#FK
-
-   isRented BOOLEAN NOT NULL DEFAULT false,#domyslnie po dodaniu do tabeli kopia nie jest wypozyczona
-   rentedTo INT(7) NOT NULL,#tutaj powinien byc FK...
-   PRIMARY KEY (copyId),
-   CONSTRAINT fk_movieInfoId FOREIGN KEY (movieInfoId) REFERENCES moviesInfo (movieInfoId)#FK definition
-);
-
 CREATE TABLE IF NOT EXISTS customers (
    customerId INT(7) NOT NULL AUTO_INCREMENT,
    fullName VARCHAR(255) NOT NULL,
@@ -31,12 +12,36 @@ CREATE TABLE IF NOT EXISTS customers (
    PRIMARY KEY (customerId)
 );
 
+CREATE TABLE IF NOT EXISTS moviesInfo (
+movieInfoId INT(7) NOT NULL AUTO_INCREMENT, #PK dla tej tabeli
+title VARCHAR(511) NOT NULL,
+genre VARCHAR(127) NOT NULL,
+releaseDate DATE NOT NULL,
+description TEXT, #uznalem ze moze byc nullem
+PRIMARY KEY (movieInfoId) #PK dla tej kolumny
+);
+
+
+CREATE TABLE IF NOT EXISTS moviesCopies(
+copyId INT(7) NOT NULL AUTO_INCREMENT,
+movieInfoId INT(7) NOT NULL, #(FK: moviesInfo -> movieInfoId)
+isRented BOOLEAN NOT NULL DEFAULT false,
+rentedTo INT(7), #(FK: customers -> customerId)
+PRIMARY KEY (copyId),
+CONSTRAINT fk_movieInfoId FOREIGN KEY(movieInfoId)
+REFERENCES moviesInfo (movieInfoId),
+CONSTRAINT fk_rentedTo FOREIGN KEY(rentedTo)
+REFERENCES customers (customerId)
+);
+
+
+
 CREATE TABLE IF NOT EXISTS rents (
    rentId INT(7) NOT NULL AUTO_INCREMENT,
    rentedMovieId INT(7) NOT NULL,
    customer INT(7) NOT NULL,
-   status ENUM ('In rent', 'Returned') NOT NULL,#tutaj moznaby dodac domyslna wartosc...
-   rentPricePerDay DECIMAL(2,2) NOT NULL,#hehehe
+   status ENUM ('In rent', 'Returned') NOT NULL DEFAULT 'Returned',
+   rentPricePerDay DECIMAL(4,2) NOT NULL,
    rentedDate DATE NOT NULL,
    returnedDate DATE,
    PRIMARY KEY (rentId),
